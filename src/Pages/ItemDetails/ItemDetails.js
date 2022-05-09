44c66e0b5817265deb22cbacdae6af240484870e
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-09 21:49:55 +0600

    itemdetils done

--- src/Pages/ItemDetails/ItemDetails.js
+++ src/Pages/ItemDetails/ItemDetails.js
@@ -1,62 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import useItemDetail from '../../hooks/useItemDetail';
 
 const ItemDetails = () => {
-    // const [quantity,setQuantity]=useState({});
     const { itemId } = useParams();
-    const [item,setItem] = useItemDetail(itemId);
+    const [items,setItems]=useState({});
+    useEffect(()=>{
+        fetch(`http://localhost:5000/item/${itemId}`)
+        .then(res=>res.json())
+        .then(data=>setItems(data))
+      },[])
 
-
-    const handleIncrease = event =>{
-        event.preventDefault();
-        const inputQuantity = event.target.name.value;
-        console.log(inputQuantity);
-       const newQuantity=parseInt(inputQuantity)+parseInt(item.quantity);
-       const newItems={...item,quantity:newQuantity};
-       console.log(newQuantity);
-       console.log(newItems);
-
-
-        // send data to the server
-        const url = `http://localhost:5000/item/${itemId}`;
-        fetch(url, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
+      const decreseitem=()=>{
+        const{_id,img,price,name,quantity,supplyName,description}=items;
+        const url=`http://localhost:5000/item/${itemId}`;
+        let value=quantity-1;
+        //send to server side
+        fetch(url,{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
             },
-            body: JSON.stringify(newItems)
+            body:JSON.stringify({quantity:value})
         })
-        .then(res => res.json())
-        .then(data =>{
-            console.log( data);
-           setItem(newItems);
-            event.target.reset();
+          .then(res=>res.json())
+          .then(data=>{
+              setItems({...items,quantity:value})
+           })
+      }
+      const handlequantity = event => {
+        const{_id,img,price,name,quantity,supplyName,description}=items;
+        event.preventDefault();
+        const qtn=parseInt(event.target.quantity.value);
+        let value=parseInt(quantity)+qtn;
+        const url=`http://localhost:5000/item/${itemId}`;
+        fetch(url,{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({quantity:value})
         })
-    }
+        .then(res=>res.json())
+        .then(data=>{
+          console.log(data)
+            setItems({...items,quantity:parseInt(quantity)+(qtn)});
+        })
+      }
 
     return (
         <div>
             <div className="container mx-auto">
-  <div className="row-12 row-md-6">
-    <div className="col align-items-end">
-    <img src={item.img} alt="" height={300} className='w-75 h-75 '/>
+  <div className="row">
+    <div className="col-12 col-lg-6 align-items-end">
+    <img src={items.img} alt="" height={300} className='w-75 h-75 '/>
     </div>
-    <div className="col  mt-3">
-    <h2 className='text-center text-success'> {item.name}</h2>
+    <div className="col-12 col-lg-6 mt-3">
+    <h2 className='text-center text-success'> {items.name}</h2>
     <hr />
-    <h3 className='text-start'>Supplier : {item.supplyName}</h3>
-           <h4 className='text-start'>Item Price : {item.price}</h4>
-           <h5 className='text-start'>Quantity : {item.quantity}</h5>
-           <p className='text-start'>{item.description}</p>
+    <h3 className='text-start'>Supplier : {items.supplyName}</h3>
+           <h4 className='text-start'>Item Price : {items.price}</h4>
+           <h5 className='text-start'>Quantity : {items.quantity}</h5>
+           <p className='text-start'>{items.description}</p>
            
            <div className='text-center'>
                
-                   <button className='btn btn-success w-100'>Deliver</button>
-                   <form className='mt-2 mb-5' onSubmit={handleIncrease}>
-                   <input type="number" className='mb-2 w-100' name="restock" id="" />
-                   <button className='btn btn-success w-100'>Restock</button>
-                   </form>
+           <button onClick={decreseitem} className='btn btn-success mb-2'>Delivery</button>
+           <form onSubmit={handlequantity}>
+         <input type="number" name="quantity" id="" placeholder='Enter some quantity' className='w-100' />
+           <input type="submit" value="Add Quantity" />
+        </form>
               
            </div>
     </div>
