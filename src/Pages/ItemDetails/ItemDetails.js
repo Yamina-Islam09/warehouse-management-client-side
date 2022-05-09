f78870ac3cde12e9807292bb08ca55ada355ef0a
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-09 21:07:44 +0600

    still item details not done

--- src/Pages/ItemDetails/ItemDetails.js
+++ src/Pages/ItemDetails/ItemDetails.js
@@ -3,49 +3,46 @@ import { useParams } from 'react-router-dom';
 import useItemDetail from '../../hooks/useItemDetail';
 
 const ItemDetails = () => {
-    const [quantity,setQuantity]=useState({});
+    // const [quantity,setQuantity]=useState({});
     const { itemId } = useParams();
-    const [item] = useItemDetail(itemId);
-      
-      useEffect( () =>{
+    const [item,setItem] = useItemDetail(itemId);
+
+
+    const handleIncrease = event =>{
+        event.preventDefault();
+        const inputQuantity = event.target.name.value;
+        console.log(inputQuantity);
+       const newQuantity=parseInt(inputQuantity)+parseInt(item.quantity);
+       const newItems={...item,quantity:newQuantity};
+       console.log(newQuantity);
+       console.log(newItems);
+
+
+        // send data to the server
         const url = `http://localhost:5000/item/${itemId}`;
-        fetch(url)
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newItems)
+        })
         .then(res => res.json())
-        .then(data => setQuantity(data));
-    }, []);
-
-    // const handleUpdateUser = event =>{
-    //     event.preventDefault();
-    //     const name = event.target.name.value;
-    //     const email = event.target.email.value;
-
-    //     const updatedUser = {name, email};
-
-    //     // send data to the server
-    //     const url = `http://localhost:5000/user/${id}`;
-    //     fetch(url, {
-    //         method: 'PUT',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(updatedUser)
-    //     })
-    //     .then(res => res.json())
-    //     .then(data =>{
-    //         console.log('success', data);
-    //         alert('users added successfully!!!');
-    //         event.target.reset();
-    //     })
-    // }
+        .then(data =>{
+            console.log( data);
+           setItem(newItems);
+            event.target.reset();
+        })
+    }
 
     return (
         <div>
-            <div class="container mx-auto">
-  <div class="row">
-    <div class="col align-items-end">
+            <div className="container mx-auto">
+  <div className="row-12 row-md-6">
+    <div className="col align-items-end">
     <img src={item.img} alt="" height={300} className='w-75 h-75 '/>
     </div>
-    <div class="col  mt-3">
+    <div className="col  mt-3">
     <h2 className='text-center text-success'> {item.name}</h2>
     <hr />
     <h3 className='text-start'>Supplier : {item.supplyName}</h3>
@@ -56,8 +53,8 @@ const ItemDetails = () => {
            <div className='text-center'>
                
                    <button className='btn btn-success w-100'>Deliver</button>
-                   <form className='mt-2 mb-5'>
-                   <input type="number" className='mb-2 w-100' name="" id="" />
+                   <form className='mt-2 mb-5' onSubmit={handleIncrease}>
+                   <input type="number" className='mb-2 w-100' name="restock" id="" />
                    <button className='btn btn-success w-100'>Restock</button>
                    </form>
               
