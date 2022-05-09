4c64525261c341854f3ad80af23b613171b03acd
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-09 14:51:51 +0600

    item details design done

--- src/Pages/ItemDetails/ItemDetails.js
+++ src/Pages/ItemDetails/ItemDetails.js
@@ -10,21 +10,22 @@ const ItemDetails = () => {
         <div>
             <div class="container mx-auto">
   <div class="row">
-    <div class="col">
-    <img src={item.img} alt="" />
+    <div class="col align-items-end">
+    <img src={item.img} alt="" height={300} className='w-75 h-75 '/>
     </div>
-    <div class="col">
-    <h2>You are about to book: {item.name}</h2>
+    <div class="col  mt-3">
+    <h2 className='text-center text-success'> {item.name}</h2>
+    <hr />
+    <h3 className='text-start'>Supplier : {item.supplyName}</h3>
+           <h4 className='text-start'>Item Price : {item.price}</h4>
+           <h5 className='text-start'>Quantity : {item.quantity}</h5>
+           <p className='text-start'>{item.description}</p>
            
-           <p>{item.quantity}</p>
-           <p>{item.description}</p>
-           <p>{item.supplyName}</p>
-           <p>{item.price}</p>
            <div className='text-center'>
                
                    <button className='btn btn-success w-100'>Sold</button>
-                   <form action="">
-                   <input type="number" name="" id="" />
+                   <form className='mt-2 mb-5'>
+                   <input type="number" className='mb-2 w-100' name="" id="" />
                    <button className='btn btn-success w-100'>Restock</button>
                    </form>
               
